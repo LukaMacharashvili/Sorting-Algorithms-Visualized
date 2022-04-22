7af31c9f100c438b9908de8ebb5e9260bb36e4d5
Luka Macharashvili <macharashvililuka12@gmail.com>
2022-04-22 09:14:07 -0700

    selection sort added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -149,22 +149,41 @@ export class AppComponent {
     }
 }
 
-  async insertionSort(){ 
+  async insertionSort(){
     this.disableBtns = true;
-    let n = this.arrayToSort.length;
-    for (let i = 1; i < n; i++) {
-        let current = this.arrayToSort[i].number;
-        let tmp = this.arrayToSort[i]
-        let j = i-1; 
-        while ((j > -1) && (current < this.arrayToSort[j].number)) {
-            this.arrayToSort[j + 1].color = 'red'
-            await this.sleep(10)
-            this.arrayToSort[j + 1].color = 'rgb(72,0,255)'
-            this.arrayToSort[j + 1] = this.arrayToSort[j];
-            j--;
+    for (let i = 0; i < this.arrayToSort.length; i++) {
+      for (let j = i; j > 0; j--) {
+          if (this.arrayToSort[j].number < this.arrayToSort[j - 1].number) {
+              this.arrayToSort[j].color = 'red';
+              await this.sleep(10);
+              [this.arrayToSort[j], this.arrayToSort[j - 1]] = [this.arrayToSort[j - 1], this.arrayToSort[j]]
+              this.arrayToSort[j].color = 'rgb(72,0,255)';
+              this.arrayToSort[j - 1].color = 'rgb(72,0,255)';
+            } else {
+              break
+          }
         }
-        this.arrayToSort[j+1] = tmp;
-        this.arrayToSort[j+1].color = 'rgb(72,0,255)'
+      }
+    }
+
+  async selectionSort() {
+    this.disableBtns = true;
+    for (let i = 0; i < this.arrayToSort.length; i++) {
+      this.arrayToSort[i].color = 'red'
+      await this.sleep(10);
+      let currentIndex = i;
+      for (let j = i; j < this.arrayToSort.length; j++) {
+          if (this.arrayToSort[currentIndex].number > this.arrayToSort[j].number) {
+            this.arrayToSort[j].color = 'yellow'
+            await this.sleep(10);  
+            currentIndex = j
+          }
+      }
+      this.arrayToSort[i].color = 'rgb(72,0,255)';
+      [this.arrayToSort[i], this.arrayToSort[currentIndex]] = [this.arrayToSort[currentIndex], this.arrayToSort[i]]
+      this.arrayToSort[i].color = 'rgb(72,0,255)' 
+      this.arrayToSort[currentIndex].color = 'rgb(72,0,255)'
     }
   }
+
 }
